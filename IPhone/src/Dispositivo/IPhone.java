9b9464119fa972cb152c2b0bc6b4d42b0727c85d
pgomezzy <palp53412@gmail.com>
2024-09-21 11:06:52 -0300

    Adição dos metodos em telefone(bruto)

--- IPhone/src/Dispositivo/IPhone.java
+++ IPhone/src/Dispositivo/IPhone.java
@@ -1,11 +1,17 @@
 package dispositivo;
 import dispositivo.musica.ReprodutorMusical;
-
+import dispositivo.telefone.AparelhoTelefonico;
 public class IPhone {
     public static void main(String[] args) throws Exception {
         ReprodutorMusical ipod = new ReprodutorMusical();
         ipod.tocar();
         ipod.selecionarMusica("Heather");
         ipod.pausar();
+        AparelhoTelefonico telefone = new AparelhoTelefonico();
+        telefone.ligar("85996004286");
+        telefone.atender();
+        telefone.iniciarCorreioVoz();
+        
+
     }
 }
