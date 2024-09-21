79c658c51e8c90579807a3509933586257d8036d
pgomezzy <palp53412@gmail.com>
2024-09-21 10:56:13 -0300

    Adição dos metodos em musica. (bruto)

--- IPhone/src/Dispositivo/IPhone.java
+++ IPhone/src/Dispositivo/IPhone.java
@@ -1,6 +1,11 @@
-package Dispositivo;
+package dispositivo;
+import dispositivo.musica.ReprodutorMusical;
+
 public class IPhone {
     public static void main(String[] args) throws Exception {
-        
+        ReprodutorMusical ipod = new ReprodutorMusical();
+        ipod.tocar();
+        ipod.selecionarMusica("Heather");
+        ipod.pausar();
     }
 }
--- IPhone/src/Dispositivo/musica/ReprodutorMusical.java
+++ IPhone/src/Dispositivo/musica/ReprodutorMusical.java
@@ -1,5 +1,17 @@
-package Dispositivo.musica;
+package dispositivo.musica;
 
-public class ReprodutorMusical {
-    
+public class ReprodutorMusical 
+{
+    public void tocar()
+    {
+        System.out.println("Dando play na musica");
+    }
+    public void pausar()
+    {
+        System.out.println("Pausando musica");
+    }
+    public void selecionarMusica(String musica)
+    {
+        System.out.println("Musica selecionada: "+ musica);
+    }
 }
